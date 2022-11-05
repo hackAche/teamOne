bb621e0157700ce71d2076d6725ff693e8bcbcbb
vinicius-munchow <viniciuslaguna.m@gmail.com>
2022-11-05 12:53:36 -0300

    feat: add mvn file structure

---
+++ src/main/java/com/hackaton/onibusUFPEL/OnibusUfpelApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.onibusUFPEL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnibusUfpelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnibusUfpelApplication.class, args);
+	}
+
+}
